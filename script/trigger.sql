73e079e697429de7ccad5044d1f38af44f2cfb31
JianJian2526 <129582563+JianJian2526@users.noreply.github.com>
2023-12-01 19:51:48 -0800

    Update trigger.sql

--- script/trigger.sql
+++ script/trigger.sql
@@ -14,3 +14,29 @@ BEGIN
     SET TotalPrice = newTotal
     WHERE OrderID = NEW.OrderID;
 END;
+
+
+--After inserting a new row into the proj.OrderDetail table (adding new order details),  the Quantity of InventoryID and InventoryLocationUsage in InventoryLocationInfo table are automatically updated.
+CREATE TRIGGER proj.UpdateInventoryAfterOrder
+ON proj.OrderDetail
+AFTER INSERT
+AS
+BEGIN
+    -- Reduce inventory quantity
+    UPDATE proj.InventoryItem
+    SET Quantity = Quantity - 1
+    FROM proj.InventoryItem
+    INNER JOIN inserted i ON proj.InventoryItem.InventoryID = i.InventoryID;
+
+    -- Update inventory usage
+    UPDATE proj.InventoryLocationInfo
+    SET InventoryLocationUsage = (
+        SELECT II.Quantity * P.InventoryUsage
+        FROM proj.Product P
+        INNER JOIN proj.InventoryItem II ON P.EANUPCCodeID = II.EANUPCCodeID
+        INNER JOIN inserted i ON II.InventoryID = i.InventoryID
+    )
+    FROM proj.InventoryLocationInfo
+    INNER JOIN proj.InventoryItem ON proj.InventoryLocationInfo.InventoryLocationID = proj.InventoryItem.InventoryLocationID
+    INNER JOIN inserted i ON proj.InventoryItem.InventoryID = i.InventoryID;
+END;
