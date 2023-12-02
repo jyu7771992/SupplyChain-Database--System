4e1f5c33499f2dcba46449f6441226fe31f53fb0
JianJian2526 <129582563+JianJian2526@users.noreply.github.com>
2023-12-01 20:15:34 -0800

    Update trigger.sql

--- script/trigger.sql
+++ script/trigger.sql
@@ -15,6 +15,8 @@ BEGIN
     WHERE OrderID = NEW.OrderID;
 END;
 
+--HouseKeeping
+DROP TRIGGER UpdateOrderTotalPrice
 
 --After inserting a new row into the proj.OrderDetail table (adding new order details),  the Quantity of InventoryID and InventoryLocationUsage in InventoryLocationInfo table are automatically updated.
 CREATE TRIGGER proj.UpdateInventoryAfterOrder
@@ -40,3 +42,40 @@ BEGIN
     INNER JOIN proj.InventoryItem ON proj.InventoryLocationInfo.InventoryLocationID = proj.InventoryItem.InventoryLocationID
     INNER JOIN inserted i ON proj.InventoryItem.InventoryID = i.InventoryID;
 END;
+
+--HouseKeeping
+DROP TRIGGER proj.UpdateInventoryAfterOrder
+    
+--If the quantity is 0, then a new OrderDetail cannot be generated
+CREATE FUNCTION proj.CheckInventoryQuantity(@InventoryID INT)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @IsAvailable BIT;
+
+    SELECT @IsAvailable = CASE 
+                              WHEN Quantity > 0 THEN 1
+                              ELSE 0 
+                           END
+    FROM proj.InventoryItem
+    WHERE InventoryID = @InventoryID;
+
+    RETURN @IsAvailable;
+END;
+
+ALTER TABLE proj.OrderDetail
+ADD CONSTRAINT chk_InventoryQuantity
+CHECK (proj.CheckInventoryQuantity(InventoryID) = 1);
+
+--HouseKeeping
+IF OBJECT_ID('proj.chk_InventoryQuantity', 'C') IS NOT NULL
+BEGIN
+    ALTER TABLE proj.OrderDetail
+    DROP CONSTRAINT chk_InventoryQuantity;
+END
+--HouseKeeping
+IF OBJECT_ID('proj.CheckInventoryQuantity', 'FN') IS NOT NULL
+BEGIN
+    DROP FUNCTION proj.CheckInventoryQuantity;
+END
+
