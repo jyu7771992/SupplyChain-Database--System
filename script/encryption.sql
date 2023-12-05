399cb44fc6cb126d77175aa1b31f71cdb06d07dc
jyu7771992 <jyu7771992@gmail.com>
2023-12-04 22:07:27 -0800

    debug and test encryption file

--- script/encryption.sql
+++ script/encryption.sql
@@ -5,7 +5,7 @@ CREATE MASTER KEY
 ENCRYPTION BY PASSWORD = 'Level_Encrypti@n';
 
 -- Create certificate to protect symmetric key
-CREATE CERTIFICATE TestCertificate
+CREATE CERTIFICATE SupplyChainCertificate
 WITH SUBJECT = 'SupplyChain Protect Certificate',
 EXPIRY_DATE = '2035-10-31';
 
@@ -22,13 +22,14 @@ DECRYPTION BY CERTIFICATE SupplyChainCertificate;
 ALTER TABLE proj.ContactInfo
 ADD EncryptedPhoneNUmber VARBINARY(256),EncryptedEmailAddress VARBINARY(256);
 
+
 --add encryption to the address info table
 ALTER TABLE proj.AddressInfo
 ADD EncryptedUnitNum VARBINARY(256), EncryptedStreetNum VARBINARY(256);
 
+
 --Encrypt the sensitive data using the symmetric key: customer
-OPEN SYMMETRIC KEY SupplyChainSymmetricKey
-  DECRYPTION BY MASTER KEY;
+
 
 UPDATE proj.ContactInfo
 SET EncryptedPhoneNUmber = EncryptByKey(Key_GUID('SupplyChainSymmetricKey'), PhoneNumber),
@@ -39,18 +40,38 @@ SET EncryptedPhoneNUmber = EncryptByKey(Key_GUID('SupplyChainSymmetricKey'), Pho
 UPDATE proj.AddressInfo
 SET EncryptedUnitNum = EncryptByKey(Key_GUID('SupplyChainSymmetricKey'), UnitNumber),
   EncryptedStreetNum = EncryptByKey(Key_GUID('SupplyChainSymmetricKey'), StreetNumber);
+ 
+ 
+ --test waht we got in the contact info & addressInfo, please make sure excute create table.sql and data.sql first
+SELECT * FROM proj.ContactInfo;
+SELECT * FROM proj.AddressInfo;
 
 
 --Decryption the sensitive data
-OPEN SYMMETRIC KEY SupplyChainSymmetricKey
-  DECRYPTION BY PASSWORD = 'Level_Encrypti@n';
 
-SELECT
-  CONVERT(NVARCHAR(50), DecryptByKey(EncryptedPhoneNUmber)) AS PhoneNumber,
-  CONVERT(NVARCHAR(50), DecryptByKey(EncryptedEmailAddress)) AS EmailAddress
+SELECT 
+  CONVERT(VARCHAR(20), DecryptByKey(EncryptedPhoneNUmber)) AS PhoneNumber,
+  CONVERT(VARCHAR(255), DecryptByKey(EncryptedEmailAddress)) AS EmailAddress
 FROM proj.ContactInfo;
 
 SELECT
-  CONVERT(NVARCHAR(50), DecryptByKey(EncryptedUnitNum)) AS UnitNumber,
-  CONVERT(NVARCHAR(50), DecryptByKey(EncryptedStreetNum)) AS StreetNumber
+  CONVERT(VARCHAR(255), DecryptByKey(EncryptedUnitNum)) AS UnitNumber,
+  CONVERT(VARCHAR(255), DecryptByKey(EncryptedStreetNum)) AS StreetNumber
 FROM proj.AddressInfo;
+
+
+-- Do housekeeping
+DROP TABLE proj.ContactInfo;
+DROP TABLE proj.AddressInfo;
+
+-- Close the symmetric key
+CLOSE SYMMETRIC KEY SupplyChainSymmetricKey;
+
+-- Drop the symmetric key
+DROP SYMMETRIC KEY SupplyChainSymmetricKey;
+
+-- Drop the certificate
+DROP CERTIFICATE SupplyChainCertificate;
+
+--Drop the DMK
+DROP MASTER KEY;
